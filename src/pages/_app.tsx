35b8434930545f85862eb30cac639fd70b1ba39a
Flynn <wphestiraid@gmail.com>
2023-06-23 14:06:47 +0900

    :construction: react-query 사용을 위한 사전 작업

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,28 @@
+import {
+  Hydrate,
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { AppProps } from 'next/app';
+import { useState } from 'react';
 
-function CustomApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+function CustomApp({
+  Component,
+  pageProps,
+}: AppProps<{ dehydratedState: unknown }>) {
+  const [queryClient] = useState(() => new QueryClient());
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      {process.env.NODE_ENV !== 'production' ? (
+        <ReactQueryDevtools initialIsOpen={false} />
+      ) : null}
+      <Hydrate state={pageProps.dehydratedState}>
+        <Component {...pageProps} />
+      </Hydrate>
+    </QueryClientProvider>
+  );
 }
 
 export default CustomApp;
