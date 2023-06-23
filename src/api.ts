7aa11ed8475eeea4d4798ebdd4b089dddd2e4bfa
Flynn <wphestiraid@gmail.com>
2023-06-23 18:19:51 +0900

    :alien: API 호출 부분 추가

---
+++ src/api.ts
@@ -0,0 +1,21 @@
+import axios from 'axios';
+
+import { API_BASE_URL } from './constants';
+
+const apiClient = axios.create({
+  baseURL: API_BASE_URL,
+});
+
+export async function getBookLists() {
+  const { data } = await apiClient.get<GetBookListsResponse>('/lists');
+  return data;
+}
+
+export async function getBookListDetail(name: string) {
+  const { data } = await apiClient.get<GetBookListDetailResponse>('/lists', {
+    params: {
+      name,
+    },
+  });
+  return data;
+}
--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,4 @@
 export const DESCRIPTION = 'Nomadcoders React Study Assignment';
 export const TITLE = 'Nomad Books';
+
+export const API_BASE_URL = 'https://books-api.nomadcoders.workers.dev';
